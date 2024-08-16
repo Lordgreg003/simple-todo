d6a9693b231cc92964a344e9a435cf4a06f9d900
chidera nwokoye <chideranwokoye555@gmail.com>
2024-08-16 10:22:56 +0100

    first commit

--- src/redux/Reducers/admin/AdmintodoReducer.ts
+++ src/redux/Reducers/admin/AdmintodoReducer.ts
@@ -7,6 +7,10 @@ import {
   ADMIN_DELETE_TODO_SUCCESS,
   ADMIN_DELETE_TODO_FAIL,
   ADMIN_DELETE_TODO_RESET,
+  ADMIN_GETBYID_TODO_REQUEST,
+  ADMIN_GETBYID_TODO_SUCCESS,
+  ADMIN_GETBYID_TODO_FAIL,
+  ADMIN_GETBYID_TODO_RESET,
 } from "../../Constants/admin/AdmintodoConstants";
 import { LOGIN_RESET } from "../../Constants/authConstants";
 import { initialState } from "../../Initial-State/initialState";
@@ -24,7 +28,7 @@ export const AdminGetAllTodoReducer = (
         ...initialState,
         loading: false,
         success: true,
-        serverResponse: action.payload,
+        serverResponse: action.payload.data,
       };
     case ADMIN_GETALL_TODO_FAIL:
       return {
@@ -54,7 +58,7 @@ export const AdminDeleteTodoReducer = (
         ...initialState,
         loading: false,
         success: true,
-        serverResponse: action.payload,
+        serverResponse: action.payload.data,
       };
     case ADMIN_DELETE_TODO_FAIL:
       return {
@@ -64,6 +68,36 @@ export const AdminDeleteTodoReducer = (
         error: action.payload,
       };
     case ADMIN_DELETE_TODO_RESET:
+    case LOGIN_RESET:
+      return { ...initialState };
+
+    default:
+      return state;
+  }
+};
+
+export const AdminGetTodoByIdReducer = (
+  state: ReduxResponseType = initialState,
+  action: ActionType
+) => {
+  switch (action.type) {
+    case ADMIN_GETBYID_TODO_REQUEST:
+      return { ...initialState, loading: true };
+    case ADMIN_GETBYID_TODO_SUCCESS:
+      return {
+        ...initialState,
+        loading: false,
+        success: true,
+        serverResponse: action.payload.data,
+      };
+    case ADMIN_GETBYID_TODO_FAIL:
+      return {
+        ...initialState,
+        loading: false,
+        success: false,
+        error: action.payload.data,
+      };
+    case ADMIN_GETBYID_TODO_RESET:
     case LOGIN_RESET:
       return { ...initialState };
 
--- src/redux/Types/admin/adminTypes.ts
+++ src/redux/Types/admin/adminTypes.ts
@@ -6,3 +6,10 @@ export type AdminGetTodoType = {
   status: boolean;
   createdAt: string;
 };
+
+export type adminCreateUserType = {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+};
